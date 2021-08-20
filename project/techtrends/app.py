2a783a3e70e1790fbce2fd959aaff85297ed4013
Madaicol <artmadai@hotmail.com>
2021-08-20 12:17:28 -0500

    All requirements of Step one are done. Small correction in globar variables.

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -18,18 +18,20 @@ port=3111
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():      
+    global db_connection_count
+    db_connection_count += 1
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
 # Function to get a post using its ID
 def get_post(post_id):    
+    global post_count
+    post_count +=1
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     connection.close()     
     return post
-
-
 
 # Define the main route of the web application 
 @app.route('/')
